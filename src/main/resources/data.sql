a2e54fc678a5470b785f61705dfb75985119e858
gustavo blanco <devgblanco@gmail.com>
2023-01-31 07:22:30 -0500

    first commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into currency_exchange (id, currency_from, currency_to, conversion_multiple, environment)
+values(10001,'USD','IRD',65,'');
+insert into currency_exchange (id, currency_from, currency_to, conversion_multiple, environment)
+values(10002,'EUR','IRD',75,'');
+insert into currency_exchange (id, currency_from, currency_to, conversion_multiple, environment)
+values(10003,'AUD','IRD',25,'');
